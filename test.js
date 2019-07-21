73ad12cc57b84f1ff825300560814e5903f4ecbb
marixer <31198560+marixer@users.noreply.github.com>
2019-07-21 13:38:11 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,6 @@
-const SerialPort = require('serialport')
-const port = new SerialPort("COM11", {
-  baudRate: 9600
-});
-
-function write(msg) //for writing
-{
-    port.on('data', function (data)
-    {
-        port.write(msg);
-    });
-}
-
-function read () // for reading
-{
-    port.on('data', function(data)
-    {
-        console.log(data);
-    });
-}
-
-port.on("open", function () {
-    port.write(0x80);
-    port.write('123456\r');
-});
+var pulsebrillo;
+intervalo = setInterval(function() {
+  var date = new Date();
+  pulsebrillo = -Math.abs(Math.sin(date.getTime()/1000))+1;
+  console.log(pulsebrillo);
+}, 1);
